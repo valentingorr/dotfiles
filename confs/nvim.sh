4b11cf625f6dec57e551cfeeba958c5c92cf05e1
Valentin Gorrin <gorrinvalentin@gmail.com>
2023-12-25 16:43:55 +0100

    remove packer.nvim clonning && fix nvim branch

--- confs/nvim.sh
+++ confs/nvim.sh
@@ -5,6 +5,4 @@ rm -rf ~/.config/nvim/
 mkdir -p ~/.config/nvim/
 packerDir="$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim"
 rm -rf $packerDir
-git clone --depth 1 https://github.com/wbthomason/packer.nvim $packerDir 
-clear
-git clone https://github.com/valentingorr/nvim-config.git -b wsl $HOME/.config/nvim/
+git clone https://github.com/valentingorr/nvim-config.git $HOME/.config/nvim/
